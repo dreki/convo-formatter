d71839e6c73bf0feb60187209a44164b3a74f7ec
Sean Gilbertson <sean.gilbertson@gmail.com>
2023-07-29 22:57:04 -0500

    📝 Simplified App component structure and removed unnecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { List } from 'antd';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { List } from "antd";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <List>
-          <List.Item>
-            <List.Item.Meta
-              title="Cool item title"
-              description="Cool item description"
-            />
-          </List.Item>
-        </List>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <List>
+                <List.Item>
+                    <List.Item.Meta
+                        title="Cool item title"
+                        description="Cool item description"
+                    />
+                </List.Item>
+            </List>
+        </div>
+    );
 }
 
 export default App;
