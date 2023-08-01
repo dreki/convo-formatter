e12e8600f8ec99812d50929db2848f539c4c6266
Sean Gilbertson <sean.gilbertson@gmail.com>
2023-07-31 21:48:07 -0500

    📝 Updated App.tsx to include file upload functionality.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
+import { useCallback } from "react";
 import "./App.css";
 
 function App() {
+    const uploadCallback = useCallback(
+        (e: React.ChangeEvent<HTMLInputElement>) => {
+            const file = e.target.files?.[0];
+            if (!file) {
+                return;
+            }
+            const reader = new FileReader();
+            reader.onload = (e) => {
+                const text = e.target?.result;
+                console.log(text);
+            };
+            reader.readAsText(file);
+        },
+        [],
+    );
+
     return (
         <div className="container mx-auto mt-8">
             <h1 className="text-xl">Upload</h1>
             <p>Upload a conversation with ChatGPT to format.</p>
             <div className="mt-4 flex justify-normal">
-                <input type="file" className="file-input" />
+                <input
+                    type="file"
+                    className="file-input"
+                    onChange={uploadCallback}
+                />
             </div>
         </div>
     );
